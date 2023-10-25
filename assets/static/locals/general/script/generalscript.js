11dea39a221a1cdb2db1598c1926aab02e7794ef
Tomide <akintomideayomide88@gmail.com>
2023-10-25 15:44:20 +0100

    initial commit

--- assets/static/locals/general/script/generalscript.js
+++ assets/static/locals/general/script/generalscript.js
@@ -1,31 +1,31 @@
+if (!sessionStorage.getItem("user_data")){
+    window.location.href = window.location.origin + '/logout'
+}
+
+
 //For the Nav Bar 
 let __user_data = JSON.parse(sessionStorage.getItem("user_data"));
 
 function pageSetup(){
-    try {
-        
-        let curl = window.location.href;
-        let curl_split = curl.split("/")
-        let acode = curl_split[curl_split.length - 1];
-        console.log("----", acode);
-        $(`[redir="/${acode}"]`).css({color:"#711dd8", fill:"#711dd8"});
+    let curl = window.location.href;
+    let curl_split = curl.split("/")
+    let acode = curl_split[curl_split.length - 1];
+    console.log("----", acode);
+    $(`[redir="/${acode}"]`).css({color:"#711dd8", fill:"#711dd8"});
 
-        $(".limited").each(function(){
-            let to = $(this).attr("to");            
-            if(!to.split(" ").includes(__user_data.user_type)){
-                $(this).remove();
-            }
-        })
-        if (__user_data.accept_status != 1){
-            $(".unsigned").css("display", "block");
+    $(".limited").each(function(){
+        let to = $(this).attr("to");            
+        if(!to.split(" ").includes(__user_data.user_type)){
+            $(this).remove();
         }
-        $(".__to_load").each(function(){
-            const toload  = $(this).attr("item");
-            $(this).text(__user_data[toload]);
-        })
-    } catch (error) {
-            
+    })
+    if (__user_data.accept_status != 1){
+        $(".unsigned").css("display", "block");
     }
+    $(".__to_load").each(function(){
+        const toload  = $(this).attr("item");
+        $(this).text(__user_data[toload]);
+    })
 }
 pageSetup();
 
@@ -35,31 +35,6 @@ $(".nav-item").click(function(){
         window.location.href = redir;
     }
 })
-
-function communicator(handler, data, callback){
-    try {
-        window.flutter_inappwebview.callHandler(handler, ...data).then(stat=>{
-            callback(fap_interpreter(stat))
-        });
-        return
-    } catch (error) {
-        // alert(error)
-        popAlert(error)
-        callback(error)
-    }
-}
-function fap_interpreter(rets){
-    retjson = {}
-    try{
-        rets.map((retitem)=>{
-            retjson[retitem[0]['type']] = retitem[0]['packet']
-        })
-        return retjson;
-    }catch (error) {
-        // alert(error)
-        return false;
-    }
-}
 
 $("#db-logout").click(function(){        
     confirmChoice({
@@ -92,4 +67,32 @@ function logout(){
         }
     }).catch(error => console.error(error))  
     
+}
+
+mobileDetected = false;
+window.addEventListener("flutterInAppWebViewPlatformReady", function(event) {
+    mobileDetected = true;
+});
+
+
+function communicator(handler, data, callback){
+    try {
+        window.flutter_inappwebview.callHandler(handler, ...data).then(stat=>{
+            callback(fap_interpreter(stat))
+        });
+        return
+    } catch (error) {
+        callback(error)
+    }
+}
+function fap_interpreter(rets){
+    retjson = {}
+    try{
+        rets.map((retitem)=>{
+            retjson[retitem[0]['type']] = retitem[0]['packet']
+        })
+        return retjson;
+    }catch (error) {
+        return false;
+    }
 }
